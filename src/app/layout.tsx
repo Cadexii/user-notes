496f8ce4a9e171ab6df10cd46ec5d6b7a6c1d261
Cadexii <cadexiipl@gmail.com>
2025-02-04 13:07:06 +0100

    create basic header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "./components/Header/Header";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,7 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <header>
-          <h1>Header</h1>
+          <Header />
         </header>
         {children}
         <footer>
