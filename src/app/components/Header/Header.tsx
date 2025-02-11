542f389c0664748788f7e53504c11cef2f927e30
Cadexii <cadexiipl@gmail.com>
2025-02-11 13:41:28 +0100

    heading style fixes

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -14,7 +14,7 @@ const Header = () => {
       </div>
       <div className={styles.profileContainer}>
         <a href="#" className={styles.profileItem}>
-          username
+          Log Out
         </a>
       </div>
     </div>
