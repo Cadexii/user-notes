8ef7d60e30d9ab617e1315ba06088eaaf3314980
Cadexii <cadexiipl@gmail.com>
2025-02-13 12:02:40 +0100

    create notes component

---
+++ src/app/components/NotesContent/NotesContent.tsx
@@ -0,0 +1,32 @@
+import styles from "./styles.module.css";
+import Note from "./Note";
+
+const NotesContent = () => {
+  return (
+    <div className={styles.notesContent}>
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
+};
+
+export default NotesContent;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Container from "./components/Container/Container";
+import NotesContent from "./components/NotesContent/NotesContent";
+
 export default function Home() {
   return (
     <div className="user-notes">
-      <h1>Home</h1>
+      <Container>
+        <NotesContent />
+      </Container>
     </div>
   );
 }
