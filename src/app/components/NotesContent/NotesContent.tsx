e4969bfea52c1e4cc76c92b405b4b52222ec9511
Cadexii <cadexiipl@gmail.com>
2025-02-14 14:32:13 +0100

    create add note button

--- src/app/components/NotesContent/NotesContent.tsx
+++ src/app/components/NotesContent/NotesContent.tsx
@@ -1,6 +1,7 @@
 import styles from "./styles.module.css";
 import Note from "./Note";
 import Container from "../Container/Container";
+import Image from "next/image";
 
 const NotesContent = () => {
   return (
@@ -22,18 +23,10 @@ const NotesContent = () => {
         <Note />
         <Note />
         <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
-        <Note />
+        <div className={styles.addNote}>
+          <Image src="add_circle.svg" alt="Add Note" width={100} height={100} />
+          <h2>Add Note</h2>
+        </div>
       </div>
     </Container>
   );
